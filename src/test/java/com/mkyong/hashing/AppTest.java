0d97c7479afd55e409b122830511afb7aa74780b
Your Name <you@example.com>
2020-01-20 22:14:45 +0700

    #TICKET fixed 2

--- src/test/java/com/mkyong/hashing/AppTest.java
+++ src/test/java/com/mkyong/hashing/AppTest.java
@@ -1,16 +1,10 @@
 package com.mkyong.hashing;
-import org.junit.*;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
     private String INPUT = "123456";
 
     @Test
@@ -23,5 +17,5 @@ public class AppTest
         String expected = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
         Assert.assertEquals(expected, App.sha256hex(INPUT));
     }
-  
+
 }
